f471afa22b94dba45fb33c06a2193621be1b6e47
Aleksander Spro <abspro@gmail.com>
2022-09-14 13:33:41 +0200

    Formatting fix

--- datatypes/enums/_ImGuiFocusedFlags.lua
+++ datatypes/enums/_ImGuiFocusedFlags.lua
@@ -7,5 +7,5 @@ ImGuiFocusedFlags = {
   RootWindow                    = 2,   -- Test from root window (top most parent of the current hierarchy)
   AnyWindow                     = 4,   -- Return true if any window is focused. Important: If you are trying to tell how to dispatch your low-level inputs, do NOT use this. Use 'io.WantCaptureMouse' instead! Please read the FAQ!
   DockHierarchy                 = 6,   -- Consider docking hierarchy (treat dockspace host as parent of docked window)
-  RootAndChildWindows           = 3         -- RootWindow | ChildWindows
+  RootAndChildWindows           = 3    -- RootWindow | ChildWindows
 }
