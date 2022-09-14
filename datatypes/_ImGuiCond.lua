156a82f64bd319cfe567c0a81dd3e4ec93aacdc4
Aleksander Spro <abspro@gmail.com>
2022-09-14 10:10:07 +0200

    Refactor imgui defs

---
+++ datatypes/_ImGuiCond.lua
@@ -0,0 +1,8 @@
+---@meta
+
+---@class ImGuiCond
+---@field public None any
+---@field public Always any
+---@field public Once any
+---@field public FirstUseEver any
+---@field public Appearing any
---
+++ datatypes/_ImVec2.lua
@@ -0,0 +1,11 @@
+---@meta
+
+---@class ImVec2
+---@field x number
+---@field y number 
+ImVec2 = {}
+
+---@param x number
+---@param y number 
+---@return ImVec2
+function ImVec2.new(x, y) end
