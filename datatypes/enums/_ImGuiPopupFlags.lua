336fb9eaa2487b305693113b3b8a0c37fa4a1d49
Aleksander Spro <abspro@gmail.com>
2022-11-07 22:22:20 +0100

    Add defs needed for context menu in table

---
+++ datatypes/enums/_ImGuiPopupFlags.lua
@@ -0,0 +1,16 @@
+---@meta
+
+---@enum ImGuiPopupFlags
+ImGuiPopupFlags = {
+  None = 0,
+  MouseButtonLeft = 0,
+  MouseButtonRight = 0,
+  MouseButtonMiddle = 0,
+  MouseButtonMask = 0,
+  MouseButtonDefault = 0,
+  NoOpenOverExistingPopup = 0,
+  NoOpenOverItems = 0,
+  AnyPopupId = 0,
+  AnyPopupLevel = 0,
+  AnyPopup = 0,
+}
