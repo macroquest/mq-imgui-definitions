962312bdb7e58cced0886f8f152a36469469239d
brainiac <brainiac2k@gmail.com>
2022-11-21 17:37:52 -0800

    Update enums

--- datatypes/enums/_ImGuiCond.lua
+++ datatypes/enums/_ImGuiCond.lua
@@ -2,9 +2,9 @@
 
 ---@enum ImGuiCond
 ImGuiCond = {
-  None = 0,
-  Always = 0,
-  Once = 0,
-  FirstUseEver = 0,
-  Appearing = 0,
+  None           = 0,  -- No condition (always set the variable), same as _Always
+  Always         = 1,  -- No condition (always set the variable)
+  Once           = 2,  -- Set the variable once per runtime session (only the first call will succeed)
+  FirstUseEver   = 4,  -- Set the variable if the object/window has no persistently saved data (no entry in .ini file)
+  Appearing      = 8,  -- Set the variable if the object/window is appearing after being hidden/inactive (or the first time)
 }
