c10654d340f5bace95c311d4ca712d8d4e72ccef
brainiac <brainiac2k@gmail.com>
2022-11-21 17:38:50 -0800

    Add ImGuiIO

---
+++ datatypes/_ImGuiIO.lua
@@ -0,0 +1,22 @@
+---@meta
+
+
+---@class ImFont
+ImFont = {}
+
+---@class ImFontAtlas
+---@field public TexID ImTextureID
+---@field public TexWidth number
+---@field public TexHeight number
+ImFontAtlas = {}
+
+---@class ImGuiIO
+---@field public DisplaySize ImVec2
+---@field public FontDefault ImFont
+---@field public Fonts ImFontAtlas
+---@field public MouseDelta ImVec2
+---@field public MousePos ImVec2
+---@field public KeyCtrl boolean
+---@field public KeyShift boolean
+---@field public KeyAlt boolean
+ImGuiIO = {}
